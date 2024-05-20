f6c5faa564a2bb36197387984e01fb16e55c09e5
Steve Nguyen <steve.nguyen.stng@gmail.com>
2024-05-19 21:46:04 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -12,9 +12,10 @@ public class Polynomial {
     }
     public Polynomial add(Polynomial x) {
         int arr_len = Math.max(coefficients.length, x.coefficients.length);
+	int arr_min_len = Math.min(coefficients.length, x.coefficients.length);
         double [] new_coefficients = new double[arr_len];
         for (int i = 0; i < arr_len; i++) {
-            if (i < Math.min(coefficients.length,x.coefficients.length)) {
+            if (i < arr_min_len) {
                 new_coefficients[i] = coefficients[i] + x.coefficients[i];
             }
             else if (coefficients.length < x.coefficients.length) {
